74829da7834775f94a812ded5449e9cc23e69730
chaofox301 <144250796+chaofox301@users.noreply.github.com>
2024-03-26 09:18:44 -0600

    commitmain

---
+++ WestWindSystem/BLL/RegionServices.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+#region Additional Namespaces
+using WestWindSystem.Entities;
+using WestWindSystem.DAL;
+using System.Reflection.Metadata.Ecma335;
+#endregion
+
+namespace WestWindSystem.BLL
+{
+    public class RegionServices
+    {
+        private readonly WestWindContext _context;
+
+        //When we instantiate this class we need the context to be provided so we know where the DB is. A service cannot do anything unless it knows where the data is.
+
+        public RegionServices(WestWindContext context)
+        {
+            _context = context;
+        }
+
+        //code the services
+
+        public Region? LookupRegionByID(int regionID)
+        {
+            return _context.Regions.Where(R=>R.RegionId==regionID).FirstOrDefault();
+            //returning a region object, or null if it doesnt find it
+        }
+    }
+}
+
---
+++ WestWindSystem/WestWindExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WestWindSystem.DAL;
+using WestWindSystem.BLL;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace WestWindSystem
+{
+    public static class WestWindExtensions
+    {
+
+        public static void WWExtensions(
+            this IServiceCollection services, Action<DbContextOptionsBuilder> options)
+        {
+            //register the context
+            services.AddDbContext<WestWindContext>(options);
+
+            //we need to AddTransient for each service class in the BBL
+            services.AddTransient<RegionServices>((ServiceProvider) =>
+            {
+                var context = ServiceProvider.GetService<WestWindContext>();
+                return new RegionServices(context);
+            });
+
+            services.AddTransient<CustomerServices>((ServiceProvider) =>
+            {
+                var context = ServiceProvider.GetService<WestWindContext>();
+                return new CustomerServices(context);
+            });
+
+
+        }
+
+
+    }
+}
